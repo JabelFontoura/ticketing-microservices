440880281873eecb55423dd0acaf595e1dcc32bf
Jabel Fontoura <jabel_fontoura@hotmail.com>
2022-07-15 21:36:13 -0300

    finish signup implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,28 +1,55 @@
-import express from 'express';
 import 'express-async-errors';
+import express from 'express';
 import config from 'config';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from '../errors/not-found-error';
+import { NotFoundError } from './errors/not-found-error';
 
-const app = express();
-const serverPort = config.get('server.port');
+const startUp = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined.');
+    }
 
-app.use(json());
+    const serverPortConfiguration = config.get('server.port');
+    const dbConnectionConfiguration = config.get('db.connectionString') as string;
 
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
+    const app = express();
+    
+    app.set('trust proxy', true);
+    app.use(json());
+    app.use(
+        cookieSession({
+            signed: false,
+            secure: true
+        })
+    );
 
-app.all('*', async (req, res, next) => { throw new NotFoundError() });
+    app.use(currentUserRouter);
+    app.use(signupRouter);
+    app.use(signinRouter);
+    app.use(signoutRouter);
 
-app.use(errorHandler);
+    app.all('*', async (req, res, next) => { throw new NotFoundError() });
 
-app.listen(serverPort, () => {
-    console.log(`Listening on port ${serverPort}!`);
-});
+    app.use(errorHandler);
+
+    try {
+        await mongoose.connect(dbConnectionConfiguration);
+    } catch (error) {
+        console.error(error);
+    }
+
+    app.listen(serverPortConfiguration, () => {
+        console.log(`Listening on port ${serverPortConfiguration}!`);
+    });
+}
+
+
+startUp();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../../errors/database-connection-error';
-import { RequestValidaitonError } from '../../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { RequestValidaitonError } from '../errors/request-validation-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -19,13 +21,21 @@ router.post('/api/users/signup', [
     if (!errors.isEmpty()) {
         throw new RequestValidaitonError(errors.array());
     }
-
     const { email, password } = req.body;
 
-    console.log('Creating a user..');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+        throw new BadRequestError('Email already in use.')
+    }
+
+    const user = User.build({ email, password});
+    await user.save();
+
+    const userJwt = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!);
+    req.session = { jwt: userJwt };
+ 
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
